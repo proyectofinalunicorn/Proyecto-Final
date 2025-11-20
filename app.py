3835e17ff6b1cbf3567ad5cbd71030f2da83b268
proyectofinalunicorn <proyectofinalunicorn@gmail.com>
2025-11-20 19:26:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -343,7 +343,7 @@ st.title("💰 Análisis de inversiones")
 st.write("Sube tu reporte de Balanz y completa los datos de tu Base de Datos de Supabase (PostgreSQL).")
 st.write("El reporte a utilizar corresponde a 'Resultados del periodo' e informe 'Completo'")
 
-if st.button("📚 Ver instructivo de descarga", help="Haz clic para ver cómo bajar el Excel de Balanz"):
+if st.button("Ver instructivo de descarga", help="Haz clic para ver cómo bajar el Excel de Balanz"):
             mostrar_instructivo()
 st.divider()
 
@@ -450,6 +450,7 @@ if submit_button:
     else:
         # Si faltan campos
         st.warning("Por favor, completa TODOS los campos y sube un archivo.")
+
 
 
 
